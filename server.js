6117f90377df53f6149f4189f9981bd5c413dc9e
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-16 21:55:57 -0500

    updating some of my GET routes

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// GET /notes route to return notes.html
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+// GET Wildcard route to direct users to index.html
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
