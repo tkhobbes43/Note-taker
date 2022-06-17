34488536243827bd8c1b036c39a026dd3187363e
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-16 21:47:33 -0500

    requiring path, express, fs and adding middleware

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
