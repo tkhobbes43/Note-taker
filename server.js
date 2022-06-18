aa01d51a7ea4d8e7353d6663941189c0634f260f
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-17 23:15:21 -0500

    updated DELETE as well

--- server.js
+++ server.js
@@ -10,6 +10,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// GET /notes route to return notes.html
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        const newNote = JSON.parse(data)
+        res.json(newNote)
+        if (err) throw err;
+    });
+});
+
+// GET Wildcard route to direct users to index.html
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// POST new note
 app.post('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
         if (err) throw err;
@@ -26,31 +45,20 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
-// GET /notes route to return notes.html
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        const notesFromFile = JSON.parse(data)
-        res.json(notesFromFile)
+app.delete('/api/notes/:id', (req, res) => {
+    const noteID = req.params.id;
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
         if (err) throw err;
+        const notes = JSON.parse(data);
+        const notesArray = notes.filter(item => {
+            return item.id !==  noteID
+        });
+        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err, data) => {
+            if (err) throw err;
+            res.json(notesArray)
+        });
     });
 });
-
-// GET Wildcard route to direct users to index.html
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-);
-
-app.delete('/api/notes/:id', (req, res) => {
-    let notes = JSON.parse(data)
-    notes = notes.filter(notes => notes.id.toString() !== req.params.id.toString());
-    fs.readFile('./db/db.json', JSON.stringify(notes));
-        res.json(notes);
-    
-})
 
 app.listen(PORT , () => {
     console.log(`the server is up now on ${PORT}!`);
